75385620944c348cf5d6939acb432417c78ff739
bbster <bbster12@naver.com>
2020-04-27 19:05:37 +0900

    create pybo list,detail

--- pybo/views.py
+++ pybo/views.py
@@ -1,5 +1,28 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+
+from pybo.forms import QuestionForm
+from pybo.models import Question
 
 
 def index(request):
-    return HttpResponse('Hello World')
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}  # dict 형태
+    return render(request, 'question_list.html', context)
+
+
+def detail(request, question_id):
+    question = Question.objects.get(id=question_id)
+    context = {'question': question}
+    return render(request, 'question_detail.html', context)
+
+
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    return redirect('pybo:detail', question_id=question_id)
+
+
+def question_create(request):
+    form = QuestionForm()
+    return render(request, 'pybo/question_form.html', {'form':form})
