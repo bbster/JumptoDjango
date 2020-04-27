8a9a101b9adda4e18a1458fc8d5b236335a0980d
bbster <bbster12@naver.com>
2020-04-27 17:13:23 +0900

    init

---
+++ pybo/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Question(models.Model):  # 장고의 모델클래스이므로 models.Model 클래스 상속
+    subject = models.CharField(max_length=200)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.subject
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField()
---
+++ readme.md
@@ -0,0 +1,23 @@
+# Wikidocs Django clone coding
+## 환경설정
+```bash
+verision
+Django==3.0.5
+django-test-plus==1.4.0
+flake8==3.7.9
+ipython==7.13.0
+mysqlclient==1.4.6
+pytest-django==3.9.0
+```
+### error 
+```bash
+ - mysql access denied user - 
+try
+    1. Database 다시생성, DB user 다시생성 - 해결안됨
+    2. 프로젝트 재생성 - 해결안됨
+    3. 포트 확인 / mysql - SHOW GLOBAL VARIABLES LIKE 'PORT'; - 해결됨
+        - 장고 세팅에 3306port / db port 3307 - 에러발생
+```
+```bash
+
+```
