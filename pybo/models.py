64d5c53277bb8e912a86e78f6faf747c4c76798b
bbster <bbster12@naver.com>
2020-05-07 17:18:51 +0900

    로그인 로그아웃 & 게시글 수정

--- pybo/models.py
+++ pybo/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -5,6 +6,8 @@ class Question(models.Model):  # 장고의 모델클래스이므로 models.Model
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    modify_date = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.subject
@@ -14,3 +17,5 @@ class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    modify_date = models.DateTimeField(null=True, blank=True)
