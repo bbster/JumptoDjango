8ed57e5f79bc8c43151ba679168072b7bda6986d
bbster <bbster12@naver.com>
2020-05-14 19:01:04 +0900

    추천기능 추가

--- pybo/views.py
+++ pybo/views.py
@@ -237,3 +237,23 @@ def index(request):
 
     context = {'question_list': page_obj}
     return render(request, 'pybo/question_list.html', context)
+
+
+@login_required(login_url='common:login')
+def vote_question(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user == question.author:
+        messages.error(request, '본인이 작성한 글은 추천할수 없습니다.')
+    else:
+        question.voter.add(request.user)
+    return redirect('pybo:detail', question_id=question.id)
+
+
+@login_required(login_url='common:login')
+def vote_answer(request, answer_id):
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.user == answer.author:
+        messages.error(request, '본인이 작성한 글은 추천할수 없습니다.')
+    else:
+        answer.voter.add(request.user)
+    return redirect('pybo:detail', question_id=answer.question.id)
